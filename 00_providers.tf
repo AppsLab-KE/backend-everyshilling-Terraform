fc1ba403d8189f71094813db7b2f1e38db14e2d7
ElsieMarion <elcymao@gmail.com>
2023-04-25 23:43:22 +0300

    deleted previous files

--- 00_providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region  = local.tf_workspace ? var.tags["region"] : local.tf_workspace_error
-  profile = format("elcy-%s", var.tags["environment"])
-  shared_credentials_files = ["~/.aws/credentials"]
-
-  default_tags {
-    tags = var.tags
-  }
-}
--- 01_data_locals.tf
+++
@@ -1,7 +0,0 @@
-data "aws_region" "current" {}
-
-locals {
-  region             = data.aws_region.current.name != "us-east-1" ? false : true
-  tf_workspace       = terraform.workspace == var.tags["environment"] ? true : false
-  tf_workspace_error = "Selected workspace not valid!"
-}
