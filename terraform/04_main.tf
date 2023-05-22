ce53102b5b273b5a35e757f426648b1e07c52d21
ElsieMarion <elcymao@gmail.com>
2023-05-22 13:07:24 +0300

    adding network acl rules

--- terraform/04_main.tf
+++ terraform/04_main.tf
@@ -1,9 +1,8 @@
-
 resource "aws_instance" "master_instance" {
   instance_type = "t2.micro"
   ami           = "ami-007855ac798b5175e"  # Replace with your desired AMI ID
   key_name      = "popo"     # Replace with your EC2 key pair name
-  subnet_id     = aws_subnet.my_subnet.id  # Replace with your subnet ID
+  subnet_id     = aws_subnet.sbnt[0].id  # Replace with your subnet ID
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
   associate_public_ip_address = true
   tags = {
@@ -15,7 +14,7 @@ resource "aws_instance" "worker_instance" {
   instance_type = "t2.micro"
   ami           = "ami-007855ac798b5175e"  # Replace with your desired AMI ID
   key_name      = "popo"     # Replace with your EC2 key pair name
-  subnet_id     = aws_subnet.my_subnet.id  # Replace with your subnet ID
+  subnet_id     = aws_subnet.sbnt[1].id  # Replace with your subnet ID
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
   associate_public_ip_address = true
   tags = {
--- terraform/tfvars/dev.tfvars
+++ terraform/tfvars/dev.tfvars
@@ -6,7 +6,7 @@ tags = {
   repo        = "https://github.com/AppsLab-KE/backend-everyshilling.git"
 }
 
-subnet_id        = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+subnet_id        = ["10.0.101.0/24", "10.0.102.0/24"]
 security_group   = "swarm-sg"
 instance_type    = "t2.micro"
 key_name         = "popo"
@@ -14,3 +14,4 @@ ami              = "i-092e94a912388140d"
 my_vpc           = "everyshilling-vpc"
 my_internet_gateway = "everyshilling-ig"
 nat_gateway_route = "0.0.0.0/0"
+subnets_cidr   =["10.0.1.0/24" , "10.0.2.0/24"]
