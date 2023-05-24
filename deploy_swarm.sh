ea18c5d1dea10abe80e93f9eeae7702d56cac5b1
ElsieMarion <elcymao@gmail.com>
2023-05-24 12:15:07 +0300

    docker swarm setup

--- deploy_swarm.sh
+++ deploy_swarm.sh
@@ -6,8 +6,16 @@ docker swarm init --advertise-addr <manager_public_ip>
 # Get the join token for worker nodes
 join_token=$(docker swarm join-token -q worker)
 
+# Get the IP address of the manager node
+manager_ip=$(hostname -I | awk '{print $1}')
+
 # Join worker nodes to the Swarm
-docker swarm join --token $join_token <manager_public_ip>:22
+for ip in "${NODE_IPS[@]}"; do
+  ssh -o "StrictHostKeyChecking=no" -i "${{ secrets.SSH_PRIVATE_KEY }}" ubuntu@$ip "docker swarm join --token $swarm_token $manager_ip:2377"
+done
+
+# # Join worker nodes to the Swarm
+# docker swarm join --token $join_token <manager_public_ip>:22
 
 # Deploy services and containers to the Swarm
 docker stack deploy --compose-file compose.yaml everyshilling
