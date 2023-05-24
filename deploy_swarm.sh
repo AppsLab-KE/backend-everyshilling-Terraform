55cc9188a25834e5d4950f491762a540b407df37
ElsieMarion <elcymao@gmail.com>
2023-05-24 10:40:02 +0300

    docker swarm setup

--- deploy_swarm.sh
+++ deploy_swarm.sh
@@ -8,3 +8,6 @@ join_token=$(docker swarm join-token -q worker)
 
 # Join worker nodes to the Swarm
 docker swarm join --token $join_token <manager_public_ip>:22
+
+# Deploy services and containers to the Swarm
+docker stack deploy --compose-file compose.yaml everyshilling
